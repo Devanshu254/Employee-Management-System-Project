faf99121a5004494f8c880a1c921363dae9ea3c7
2370823 <devanshujha8305@gmail.com>
2025-01-16 14:32:29 +0530

    Created Employee JPA Entity

---
+++ ems-backend/src/main/java/com/cts/ems/entity/Employee.java
@@ -0,0 +1,33 @@
+package com.cts.ems.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // It uses data base auto increment feature.
+	private Long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email_id")
+	private String email;
+}
